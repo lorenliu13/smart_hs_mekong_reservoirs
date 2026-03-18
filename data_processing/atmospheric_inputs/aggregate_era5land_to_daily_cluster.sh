1c4d66b3ea503b989b2744e2f2750d4302d07465
lorenliu13 <binibear9988@163.com>
2026-03-18 10:18:57 +0000

    1

---
+++ data_processing/atmospheric_inputs/aggregate_era5land_to_daily_cluster.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=aggregate_era5land_to_daily_cluster          # Name of your job
+#SBATCH --output=aggregate_era5land_to_daily_cluster.out        # Standard output file
+#SBATCH --error=aggregate_era5land_to_daily_cluster.err         # Standard error file
+#SBATCH --nodes=1                         # Request one node
+#SBATCH --ntasks=1                        # Request one task
+#SBATCH --cpus-per-task=20                # Request 10 CPU per task
+#SBATCH --mem-per-cpu=4G                  # Request 4GB memory per CPU
+#SBATCH --time=04:00:00                   # Job run time (1 hour)
+#SBATCH --partition=short                 # Specify the partition/queue
+
+# Load any necessary modules
+module load Anaconda3
+conda activate $DATA/py311
+
+# Run the download script
+python aggregate_era5land_to_daily_cluster.py
+
