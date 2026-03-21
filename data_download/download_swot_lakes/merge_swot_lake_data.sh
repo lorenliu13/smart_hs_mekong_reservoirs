7c3060c45f6a4380d96c1d40b69fe21596c711d4
lorenliu13 <binibear9988@163.com>
2026-03-21 15:35:41 +0000

    1

--- data_download/download_swot_lakes/merge_swot_lake_data.sh
+++ data_download/download_swot_lakes/merge_swot_lake_data.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1                         # Request one node
 #SBATCH --ntasks=1                        # Request one task
 #SBATCH --cpus-per-task=3                # Request 10 CPUs per task
-#SBATCH --mem-per-cpu=4G                  # Request 4GB memory per CPU
+#SBATCH --mem-per-cpu=8G                  # Request 4GB memory per CPU
 #SBATCH --time=4:00:00                   # Job run time (4 hours)
 #SBATCH --partition=short                 # Specify the partition/queue
 
