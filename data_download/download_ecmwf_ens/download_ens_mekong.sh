928d911112ba4caa8c7e71ec7b5cd4f32ac6f60d
lorenliu13 <binibear9988@163.com>
2026-04-22 20:12:09 +0100

    1

--- data_download/download_ecmwf_ens/download_ens_mekong.sh
+++ data_download/download_ecmwf_ens/download_ens_mekong.sh
@@ -6,12 +6,12 @@
 #SBATCH --ntasks=1                        # Request one task
 #SBATCH --cpus-per-task=1                # Request 1 CPUs per task
 #SBATCH --mem-per-cpu=4G                  # Request 4GB memory per CPU
-#SBATCH --time=144:00:00                   # Job run time (72 hours)
+#SBATCH --time=168:00:00                   # Job run time (72 hours)
 #SBATCH --partition=long                 # Specify the partition/queue
 
 # Load any necessary modules
 module load Anaconda3
-source activate $DATA/py311
+conda activate $DATA/py311
 
 # Run the download script
-python download_ens_mekong.py
+python -u download_ens_mekong.py
