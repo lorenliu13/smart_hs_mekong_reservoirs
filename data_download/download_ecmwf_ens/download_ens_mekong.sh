3fb62952f07ca91914f12fe73aecc0c8fbd983d8
lorenliu13 <binibear9988@163.com>
2026-04-27 23:22:53 +0100

    1

--- data_download/download_ecmwf_ens/download_ens_mekong.sh
+++ data_download/download_ecmwf_ens/download_ens_mekong.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1                        # Request one task
 #SBATCH --cpus-per-task=1                # Request 1 CPUs per task
 #SBATCH --mem-per-cpu=4G                  # Request 4GB memory per CPU
-#SBATCH --time=336:00:00                   # Job run time (72 hours)
+#SBATCH --time=168:00:00                   # Job run time (72 hours)
 #SBATCH --partition=long                 # Specify the partition/queue
 
 # Load any necessary modules
